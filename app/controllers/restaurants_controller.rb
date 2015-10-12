e2b7c050fb55a91e5a6691bd52fb5dd2d1d890ec
Dom Batten <dominic.batten@googlemail.com>
2015-10-12 16:33:32 +0100

    new restaurant page working, all tests passing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,12 +4,16 @@ class RestaurantsController < ApplicationController
 		@restaurants = Restaurant.all
 	end
 
-	def new
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
 
+	def new
+		@restaurant = Restaurant.new
 	end
 
 	def create
-		Restaurant.create(restaurant_params)
+		@restaurant = Restaurant.create(restaurant_params)
 		redirect_to '/restaurants'
 	end
 
