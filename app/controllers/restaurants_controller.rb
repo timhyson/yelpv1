1ad2d0b5f78c6ef8b820bc3f6580ded3d6d1a706
Tim Hyson <tjhyson@gmail.com>
2015-10-12 16:07:07 +0100

    new restaurant can be added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,6 +8,12 @@ class RestaurantsController < ApplicationController
 
 	end
 
+	def create
+		Restaurant.create(restaurant_params)
+		redirect_to '/restaurants'
+	end
 
-
+	def restaurant_params
+		params.require(:restaurant).permit(:name)
+	end
 end
